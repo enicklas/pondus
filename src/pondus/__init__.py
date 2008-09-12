d6cd8b1827746984d2c527d046afb89ac1c05378
Eike Nicklas <eike@ephys.de>
2008-09-12 09:13:12 +0200

    post release version bump

--- src/pondus/__init__.py
+++ src/pondus/__init__.py
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 __all__ = ['core', 'gui', 'parameters', 'user_data']
-__version__ = '0.5.1'
+__version__ = '0.5.1+hg'
 
 import gettext
 import os
