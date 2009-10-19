aa346f4849957e2687372513ce9ecec2fd7eee63
Eike Nicklas <eike@ephys.de>
2009-10-19 21:33:29 +0200

    switch from GPL to MIT license

--- pondus/gui/__init__.py
+++ pondus/gui/__init__.py
@@ -4,18 +4,8 @@
 This file is part of Pondus, a personal weight manager.
 Copyright (C) 2007-09  Eike Nicklas <eike@ephys.de>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+This program is free software licensed under the MIT license. For details
+see LICENSE or http://www.opensource.org/licenses/mit-license.php
 """
 
 __all__ = ['dialog_add', 'dialog_csv', 'dialog_message', 'dialog_plot',
