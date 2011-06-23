e2ce0e0e8b48df6a91e9b7c148fe2765461d42a6
Eike Nicklas <eike@ephys.de>
2011-06-23 15:40:12 +0200

    update documentation about im-/export

--- pondus/gui/__init__.py
+++ pondus/gui/__init__.py
@@ -2,12 +2,12 @@
 
 """
 This file is part of Pondus, a personal weight manager.
-Copyright (C) 2007-10  Eike Nicklas <eike@ephys.de>
+Copyright (C) 2007-11  Eike Nicklas <eike@ephys.de>
 
 This program is free software licensed under the MIT license. For details
 see LICENSE or http://www.opensource.org/licenses/mit-license.php
 """
 
-__all__ = ['dialog_add', 'dialog_csv', 'dialog_message', 'dialog_plot',
+__all__ = ['dialog_add', 'dialog_imexport', 'dialog_message', 'dialog_plot',
            'dialog_preferences', 'dialog_save_file', 'dialog_select_file',
            'guiutil', 'window_main']
